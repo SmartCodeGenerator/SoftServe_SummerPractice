a0b51b1525c6016e1410f59dd113dc001f023c4c
Alex <alexjfr112@gmail.com>
2020-06-30 15:45:36 +0300

    Дичайше, звонко и по красоте

--- Module2/module2_homework.rb
+++
@@ -1,9 +0,0 @@
-class Mathematics
-    def power
-        numbers = gets.chomp
-        numbers = numbers.split(" ")
-        osnova, stepin = numbers[0].to_f, numbers[1].to_f
-
-        puts osnova ** stepin
-    end
-end
